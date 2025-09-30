d4c5b58b4ada4d695602a6b05a93d53b69ee2faa
ahsan uddin <ahsan.uddin@sonalibank.com.bd>
2025-09-30 16:18:36 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import Header from './components/Header/Header';
+import './App.css'
+import Blogs from './components/Blogs/Blogs';
+
+function App() {
+  
+  return (
+    <>
+
+     <Header></Header>
+     <Blogs></Blogs>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const Blogs = () => {
+
+    // eslint-disable-next-line no-unused-vars
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, []);
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
