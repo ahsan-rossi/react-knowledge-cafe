b4f12e8b927cb69e38ffd83cb08a71fe8a0484aa
ahsan uddin <ahsan.uddin@sonalibank.com.bd>
2025-10-08 12:39:03 +0600

    updated

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
 
@@ -16,6 +17,12 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
            <h1 className='text-4xl' >Blogs</h1> 
+           {
+            blogs.map(blog => <Blog 
+                key={blog.id} 
+                blog={blog}
+            ></Blog>)
+           }
         </div>
     );
 };
